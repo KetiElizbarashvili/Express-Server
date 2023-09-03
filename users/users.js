cb064a397ed5d5d2e94d3d25f5880ce26494c0c7
KetiElizbarashvili <elizbarashviliketi44@gmail.com>
2023-09-03 22:16:46 +0400

    newchanges

--- users/users.js
+++ users/users.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router=express.Router()
-const usersEmailContainer = [{email:"nika.chaduneli.com@gmail.com",name:"Nigorozi"}]
+const usersEmailContainer = [{email:"elizbarashviliketi44@gmail.com",name:"keti"}]
 
 
 router.post('/create-email', (req,res)=>{
